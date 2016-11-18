42c48c7b5c231446e677a2aaf1b841cd907663d2
Alan Thomas <alan.q.thomas@gmail.com>
2016-11-17 21:41:28 -0500

    Add comments

--- cpp-double-linked-list/Queue.h
+++ cpp-double-linked-list/Queue.h
@@ -8,52 +8,64 @@
 template<class DataType>
 class Queue {
 	
+	// Use a DoubleLinkedList to implement Queue
 	private:
 		DoubleLinkedList<DataType> list;
 
 public:
+	// Default constructor
 	Queue() { }
 
+	// Destructor
 	~Queue() {
 		std::cout << "Queue destructor called. \n";
 	}
 
+	// Return whether the queue is empty or not
 	bool empty() const {
 		return list.empty();
 	}
 
+	// Returns the size of the queue
 	std::size_t size() const {
 		return list.size();
 	}
 
+	// Returns the front item in the queue
 	DataType& front() {
 		DataType& value = list.first()->data();
 		return value;
 	}
 
+	// Returns the front item in the queue
 	const DataType& front() const {
 		const DataType& value = list.first()->data();
 		return value;
 	}
 
+	// Returns the back item in the queue
 	DataType& back() {
 		DataType& value = list.last()->data();
 		return value;
 	}
 
+	// Returns the back item in the queue
 	const DataType& back() const {
 		const DataType& value = list.last()->data();
 		return value;
 	}
 
+	// Adds the item to the back of the queue
 	void push(const DataType& value) {
 		list.push_back(value);
 	}
 
+	// Removes the front item in the queue
 	void pop() {
 		list.pop_front();
 	}
 
+	// Prints a representation of the queuue
 	void print() {
 		list.print();
 	}
--- cpp-double-linked-list/Stack.h
+++ cpp-double-linked-list/Stack.h
@@ -8,43 +8,52 @@
 template<class DataType>
 class Stack {
 
+	// Use a DoubleLinkedList to implement Stack
 	private:
 		DoubleLinkedList<DataType> list;
 
 	public:
-
+		// Default constructor
 		Stack() { }
 		
+		// Destructor
 		~Stack() {
 			std::cout << "Stack destructor called.\n";
 		}
 
+		// Return whether the stack is empty or not
 		bool empty() const {
 			return list.empty();
 		}
 
+		// Returns the size of the stack
 		std::size_t size() const {
 			return list.size();
 		}
 
+		// Returns the top item in the stack
 		DataType& top() {
 			DataType& value = list.last()->data();
 			return value;
 		}
 
+		// Returns the top item in the stack
 		const DataType& top() const {
 			const DataType& value = list.last()->data();
 			return value;
 		}
 
+		// Pushes an item to the top of the stack
 		void push(const DataType& value) {
 			list.push_back(value);
 		}
 
+		// Removes the top item in the stack
 		void pop() {
 			list.pop_back();
 		}
 
+		// Prints a representation of the stack
 		void print() {
 			list.print();
 		}
